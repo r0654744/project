6d198b20ad31a249673d7b3020f4e2c1b37e4fbb
r0654744 <33875054+r0654744@users.noreply.github.com>
2017-11-29 15:28:42 +0100

    Delete saveproduct.php

--- inventory/saveproduct.php
+++
@@ -1,8 +0,0 @@
-<?php
-include("db.php");
-$proname=$_POST['proname'];
-$price=$_POST['price'];
-$qty=$_POST['qty'];
-mysql_query("INSERT INTO inventory (item, price, qtyleft) VALUES ('$proname', '$price','$qty')");
-header("location: tableedit.php#page=addpro");
-?>
