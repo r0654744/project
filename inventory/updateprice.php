6e38eb49881a1022f37d14ba808d55faa96c1586
r0654744 <33875054+r0654744@users.noreply.github.com>
2017-11-29 10:57:25 +0100

    Delete updateprice.php

--- inventory/updateprice.php
+++
@@ -1,8 +0,0 @@
-<?php
-include("db.php");
-$proid=$_POST['ITEM'];
-$itemprice=$_POST['itemprice'];
-mysql_query("UPDATE inventory SET price='$itemprice'
-WHERE id='$proid'");
-header("location: tableedit.php#page=editprice");
-?>
