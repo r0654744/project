11ddbbdbba70f58f1bc646a5fc1ced236e2643c7
r0654744 <33875054+r0654744@users.noreply.github.com>
2017-11-29 10:28:15 +0100

    Update and rename liveedit.sql to docenten.sql

---
+++ docenten.sql
@@ -0,0 +1,13 @@
+CREATE TABLE IF NOT EXISTS 'docenten' (
+  'id' int(11) NOT NULL,
+  'name' varchar(100) NOT NULL,
+  'surname' varchar(100) NOT NULL,
+  PRIMARY KEY ('id')
+);
+
+INSERT INTO 'inventory' (`id`, ) VALUES
+(1, 'Peggy', 'Houdmeyers'),
+(2, 'Yvan', 'Joris'),
+(3, 'Kenny', 'Joris');
+
+
