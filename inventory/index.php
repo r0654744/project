62282a68b88930e37db1a6cb6e689ee0fb7d6133
r0654744 <33875054+r0654744@users.noreply.github.com>
2017-11-29 10:23:32 +0100

    Update index.php

--- inventory/index.php
+++ inventory/index.php
@@ -5,7 +5,6 @@ $db = new mysqli('localhost', 'root', 'demo', 'demodb');
 
 if($db->connect_errno > 0) {
    die('Unable to connect to database [' . $db->connect_error . ']');
-   echo 'Kapot!';
 }
 
 $sql = 'SELECT * FROM docenten';
