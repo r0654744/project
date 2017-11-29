afc9f54d117f2fa889b2b2bfe09aa2470fc096be
r0654744 <33875054+r0654744@users.noreply.github.com>
2017-11-29 10:34:38 +0100

    Update docenten.sql

--- docenten.sql
+++ docenten.sql
@@ -1,13 +1,11 @@
-CREATE TABLE IF NOT EXISTS 'docenten' (
-  'id' int(11) NOT NULL,
-  'name' varchar(100) NOT NULL,
-  'surname' varchar(100) NOT NULL,
-  PRIMARY KEY ('id')
+CREATE TABLE IF NOT EXISTS `docenten` (
+  `id` int(11) NOT NULL,
+  `name` varchar(100) NOT NULL,
+  `surname` varchar(100) NOT NULL,
+  PRIMARY KEY (`id`)
 );
 
-INSERT INTO 'inventory' (`id`, ) VALUES
+INSERT INTO `docenten` (`id`, `name`,`surname`) VALUES
 (1, 'Peggy', 'Houdmeyers'),
 (2, 'Yvan', 'Joris'),
 (3, 'Kenny', 'Joris');
-
-
